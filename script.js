506d6a43ff30ca36b392e26ab51213a6c74d679f
Rok Slana <rokslana@gmail.com>
2022-09-15 12:22:11 +0200

    updated version of Rock-Scissors-Paper game

--- script.js
+++ script.js
@@ -3,30 +3,49 @@ const buttons = document.querySelectorAll('button');
 let computerWins = 0;
 let playerWins = 0;
 
+//COMPUTER MAKES A GAMBLE
 function getComputerChoice(){    
     return weapons[Math.floor(Math.random()*3)];    
 }
-
+//PLAYER GIVES HIS BEST
 function playerInput(e){    
     let playerSelection = this.innerText;
     let computerSelection = getComputerChoice();
     playSingleGame(playerSelection, computerSelection);
 }
 
+//TAKES THE ABOVE AND EVALUATES RESULT
 function playSingleGame(player, computer){
     if( player == computer){
-        console.log('It\'s a tie');
+        document.getElementById('winner').innerText = 'IT\S A TIE';
     }else if(player == 'ROCK' && computer == 'SCISSORS' || player == 'SCISSORS' && computer == 'PAPER' || player == 'PAPER' && computer == 'ROCK'){
-        console.log("you win");
+        document.getElementById('winner').innerText = 'YOU WON';
         playerWins++;
     }else{
-        console.log('you loose');
+        document.getElementById('winner').innerText = 'ZEE MACHINE WON';
         computerWins++;
     }
-    console.log('PLAYER SELECTEED : ' + player);
-    console.log('COMPUTER SELECTEED : ' + computer);
-    console.log('Current score : Player: ' + playerWins +' // Computer : ' + computerWins)
 
+    updateScore(playerWins, computerWins);
 }
-buttons.forEach(button => button.addEventListener('click', playerInput));
 
+//SCORE UPDATER
+function updateScore(player,computer){
+    document.getElementById('score').innerText = player + " : " + computer;
+    //CHECK IF GAME IS OVER YET WITH TIME DELAY
+    window.setTimeout(checkScore, 1500);
+}
+
+//GAME OVER CHECK HERE
+function checkScore(){
+    if(playerWins==5){
+        alert('YOU WON! UNBELIEVABLE !');
+        location.reload();
+    }else if(computerWins == 5){
+        alert('ZEE MACHINE WON! So sad. Try again')
+        location.reload();
+    }
+}
+
+//ATTACH EVENT LISTENERS TO ALL THE BUTTONS HERE
+buttons.forEach(button => button.addEventListener('click', playerInput));
